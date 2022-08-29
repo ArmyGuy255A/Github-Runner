b7a316d6af4880f85048bda20dfd68eb3eccf012
Phil Dieppa <mrdieppa@gmail.com>
2022-08-28 21:21:00 -0700

    asdf

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-docker build --build-arg RUNNER_VERSION=2.285.1 --tag armyguy255a/github-runner:2.285.1 .
+docker build --build-arg RUNNER_VERSION=2.285.1 --tag armyguy255a/github-runner:2.285.1 --tag armyguy255a/github-runner:latest .
