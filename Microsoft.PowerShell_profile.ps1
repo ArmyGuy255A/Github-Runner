f2c2886f14d29d1e7aaf0af74f6f69520c59274f
Phil Dieppa <mrdieppa@gmail.com>
2022-08-28 22:42:19 -0700

    Added some PowerShell profile helper methods for GATech.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+function Get-DockerProcessInfo () {
+    [array]$results = [array](docker ps --format "{{json . }}" | convertfrom-json)
+    return $results
+  }
+  
+  function Start-GATechRunner ($Repository) {
+    docker run -e GH_PAT='<<put your PAT here>>' -e GH_OWNER='<<username>>' -e GH_REPOSITORY=$Repository -e GH_API_URL='https://github.gatech.edu/api/v3/repos' -e GH_URL='https://github.gatech.edu' --name $Repository -d armyguy255a/github-runner:latest
+  }
+  
+  function Stop-GATechRunner ($Repository) {
+    $dockerProcesses = Get-DockerProcessInfo 
+    if ($dockerProcesses.Count -gt 1) {
+      $dockerProcesses = $dockerProcesses | Where-Object $_.Names -eq $Repository
+    }
+    docker stop $dockerProcesses.ID && docker rm $dockerProcesses.ID
+  }
