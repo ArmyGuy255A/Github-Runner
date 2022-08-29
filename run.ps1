9f13754a090abc3d8a9143c4f80d6712f004b686
Phil Dieppa <mrdieppa@gmail.com>
2022-08-28 22:48:22 -0700

    stopped tracking run.ps1

--- run.ps1
+++
@@ -1,3 +0,0 @@
-docker run -e GH_PAT='' -e GH_OWNER='' -e GH_REPOSITORY='' -e GH_API_URL='' -e GH_URL='' github-runner:gatech
-# api_url = https://github.gatech.edu/api/v3/repos
-# gh_url = https://github.gatech.edu
